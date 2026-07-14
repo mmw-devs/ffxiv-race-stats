6da681783c079f5fecf903aa61870fb030bcf2e2
weunimix <weunimix@users.noreply.github.com>
2026-07-14 16:28:22 +0800

    content: 更新[队伍名2] CLEAR→P4 (HP 0.0%→20.0%)

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ const RACE_DATA = {
       ]
     },
     {
-      id: "t2", name: "[队伍名 2]", rank: 2, bossHP: 0.0, phase: "CLEAR", region: "NA", isLive: true,
+      id: "t2", name: "[队伍名 2]", rank: 2, bossHP: 20.0, phase: "P4", region: "NA", isLive: true,
       players: [
         { job: "DRK", role: "tank", stream: "#", streaming: true },
         { job: "GNB", role: "tank", stream: "#", streaming: false },
